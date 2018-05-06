ca0b399c5d11b4f57ea133f75af3e4a969405549
last <ramses.aldama@gmail.com>
2018-05-06 06:34:56 +0200

    Save the text in the <textarea> in the *Local Storage*

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,11 @@
 import 'dart:html';
+import 'dart:convert' show json;
 
 TextAreaElement theEditor;
 
 void main() {
   theEditor = querySelector("#editor");
+  theEditor
+    ..onKeyUp.listen((_) =>
+        window.localStorage["MyTextEditor"] = json.encode(theEditor.value));
 }
