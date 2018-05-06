af98f3d852ecf935eb3adeb36c4df0bf1c6a3eeb
last <ramses.aldama@gmail.com>
2018-05-06 17:27:22 +0200

    Load the text store in *Local Storage* in to the <textarea> tag

--- web/main.dart
+++ web/main.dart
@@ -6,6 +6,15 @@ TextAreaElement theEditor;
 void main() {
   theEditor = querySelector("#editor");
   theEditor
+    ..value = loadDocument()
     ..onKeyUp.listen((_) =>
         window.localStorage["MyTextEditor"] = json.encode(theEditor.value));
+}
+
+String loadDocument() {
+  String readings = "";
+  String jsonString = window.localStorage["MyTextEditor"];
+  if (jsonString != null && jsonString.length > 0)
+    readings = json.decode(jsonString);
+  return readings;
 }
